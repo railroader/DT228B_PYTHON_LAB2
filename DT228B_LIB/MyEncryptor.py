a080c8991eceeab10f2b3ef55fab1381add8435e
railroader <terryslattery@me.com>
2016-02-14 15:57:24 +0000

    14 FEB CHANGES
    Commenting

--- DT228B_LIB/MyEncryptor.py
+++ DT228B_LIB/MyEncryptor.py
@@ -1,97 +1,113 @@
 # THIRD PARTY LIBRARY FOR THE SHA3 IMPORT WAS TAKEN FROM https://pypi.python.org/pypi/pysha3/
 # TO USE - FROM THE TERMINAL IN THE DOWNLOADED DIRECTORY TYPE
-
 # python setup.py install
-
 # THEN ADD A REFERENCE TO THE LIBRARY VIA EXTERNAL LIBRARIES IN JET BRAINS PYCHARM
 import hashlib
 import sha3
 import os.path
 
 
-
-# ENCRYPTION CLASS THAT WILL HASH A FILE OR A STRING USING VARIOUS HASHING ALGORITHMS
+# REUSABLE ENCRYPTION OBJECT THAT WILL HASH A FILE OR A STRING USING VARIOUS HASHING ALGORITHMS
 # EACH METHOD DOES THE EXACT SAME THING USING A DIFFERENT TYPE OF ENCODING
 class MyEncryptor():
     # CLASS CONSTRUCTOR
-	def __init__(self): pass
+    def __init__(self):
+        pass
 
-# METHOD TO SHA1 ENCODE OBJECT - WHICH CAN BE A FILE OR A STRING
-# FIRST CHECKS IF THE ARGUMENT IS A FILE
-#Also casts the value of the argument to a string
-# IF ITS NOT A FILE IT CHECKS IF ITS A STRING
-# IF IT IS A STRING THEN IT SHA1 ENCODES THE STRING
-# OTHERWISE IT TREATS THE ARGUMENT AS A FILE AND SHA1 ENCODES THE FILE
+    # METHOD TO SHA1 ENCODE OBJECT - WHICH CAN BE A FILE OR A STRING
+    # FIRST CHECKS IF THE ARGUMENT IS A FILE
+    # Also casts the value of the argument to a string
+    # IF ITS NOT A FILE IT CHECKS IF ITS A STRING
+    # IF IT IS A STRING THEN IT SHA1 ENCODES THE STRING
+    # OTHERWISE IT TREATS THE ARGUMENT AS A FILE AND SHA1 ENCODES THE FILE
 
-	def sha1Encode(self,text):
-		if not os.path.isfile(str(text)):
-			if not text:
-				return "No String passed to method"
-			else:
-				m = hashlib.sha1()
-				m.update(text.encode('utf-8'))
-				return (m.hexdigest())
-		else:
-			BLOCKSIZE = 65536
-			hasher = hashlib.sha1()
-			with open(text, 'rb') as afile:
-				buf = afile.read(BLOCKSIZE)
-				while len(buf) > 0:
-					hasher.update(buf)
-					buf = afile.read(BLOCKSIZE)
-			return(hasher.hexdigest())
+    def sha1Encode(self, text):
+        if not os.path.isfile(str(text)):  # check if ist a file
+            if not text:  # check if its a string
+                return "No String passed to method"
+            else:
+                m = hashlib.sha1()  # create new sha1 object
+                m.update(text.encode('utf-8'))  # call udate method passing the 'utf-8' as an argument
+                return (m.hexdigest())  # return the hash by calling hexdigest Method which retuns the hash value
+        else:
+            BLOCKSIZE = 65536  # set the block size see : http://www.dhillonblog.com/2014/07/understanding-io-request-block-size/
+            hasher = hashlib.sha1()
+            with open(text, 'rb') as afile:  # open file in read byte mode
+                buf = afile.read(BLOCKSIZE)  # create a buffer of size blocksize call the file.read method
+                while len(buf) > 0:  # while the length of the buffer is greater thena zero keep iterating
+                    hasher.update(buf)  # append buffer to the cuurent value in the hasher object
+                    buf = afile.read(BLOCKSIZE)
+            return (
+            hasher.hexdigest())  # finally return the hash by calling hexdigest Method which retuns the hash value
 
+    def sha256Encode(self, text):
+        if not os.path.isfile(str(text)):
+            if not text:
+                return "No String passed to method"
+            else:
+                m = hashlib.sha256()
+                m.update(text.encode('utf-8'))
+                return (m.hexdigest())
+        else:
+            BLOCKSIZE = 65536
+            hasher = hashlib.sha256()
+            with open(text, 'rb') as afile:
+                buf = afile.read(BLOCKSIZE)
+                while len(buf) > 0:
+                    hasher.update(buf)
+                    buf = afile.read(BLOCKSIZE)
+            return (hasher.hexdigest())
 
-	def sha256Encode(self,text):
-		if not os.path.isfile(str(text)):
-			if not text:
-				return "No String passed to method"
-			else:
-				m = hashlib.sha256()
-				m.update(text.encode('utf-8'))
-				return (m.hexdigest())
-		else:
-			BLOCKSIZE = 65536
-			hasher = hashlib.sha256()
-			with open(text, 'rb') as afile:
-				buf = afile.read(BLOCKSIZE)
-				while len(buf) > 0:
-					hasher.update(buf)
-					buf = afile.read(BLOCKSIZE)
-			return(hasher.hexdigest())
+    def sha512Encode(self, text):
+        if not os.path.isfile(str(text)):
+            if not text:
+                return "No String passed to method"
+            else:
+                m = hashlib.sha512()
+                m.update(text.encode('utf-8'))
+                return (m.hexdigest())
+        else:
+            BLOCKSIZE = 65536
+            hasher = hashlib.sha512()
+            with open(text, 'rb') as afile:
+                buf = afile.read(BLOCKSIZE)
+                while len(buf) > 0:
+                    hasher.update(buf)
+                    buf = afile.read(BLOCKSIZE)
+            return (hasher.hexdigest())
 
-	def sha512Encode(self,text):
-		if not os.path.isfile(str(text)):
-			if not text:
-				return "No String passed to method"
-			else:
-				m = hashlib.sha512()
-				m.update(text.encode('utf-8'))
-				return (m.hexdigest())
-		else:
-			BLOCKSIZE = 65536
-			hasher = hashlib.sha512()
-			with open(text, 'rb') as afile:
-				buf = afile.read(BLOCKSIZE)
-				while len(buf) > 0:
-					hasher.update(buf)
-					buf = afile.read(BLOCKSIZE)
-			return(hasher.hexdigest())
+    def sha3_256Encode(self, text):
+        if not os.path.isfile(str(text)):
+            if not text:
+                return "No String passed to method"
+            else:
+                m = hashlib.sha3_256()
+                m.update(text.encode('utf-8'))
+                return (m.hexdigest())
+        else:
+            BLOCKSIZE = 65536
+            hasher = hashlib.sha3_256()
+            with open(text, 'rb') as afile:
+                buf = afile.read(BLOCKSIZE)
+                while len(buf) > 0:
+                    hasher.update(buf)
+                    buf = afile.read(BLOCKSIZE)
+            return (hasher.hexdigest())
 
-	def sha3Encode(self,text):
-		if not os.path.isfile(str(text)):
-			if not text:
-				return "No String passed to method"
-			else:
-				m = hashlib.sha3_512()
-				m.update(text.encode('utf-8'))
-				return (m.hexdigest())
-		else:
-			BLOCKSIZE = 65536
-			hasher = hashlib.sha3_512()
-			with open(text, 'rb') as afile:
-				buf = afile.read(BLOCKSIZE)
-				while len(buf) > 0:
-					hasher.update(buf)
-					buf = afile.read(BLOCKSIZE)
-			return(hasher.hexdigest())
+    def sha3_512Encode(self, text):
+        if not os.path.isfile(str(text)):
+            if not text:
+                return "No String passed to method"
+            else:
+                m = hashlib.sha3_512()
+                m.update(text.encode('utf-8'))
+                return (m.hexdigest())
+        else:
+            BLOCKSIZE = 65536
+            hasher = hashlib.sha3_512()
+            with open(text, 'rb') as afile:
+                buf = afile.read(BLOCKSIZE)
+                while len(buf) > 0:
+                    hasher.update(buf)
+                    buf = afile.read(BLOCKSIZE)
+            return (hasher.hexdigest())
--- Main.py
+++ Main.py
@@ -1,16 +1,21 @@
 # REFERENCE TO THE CLASS IN DT228B_Lib package
+import sys
+import os
+# Reference Class Library DT228b_lib
 from DT228B_LIB.MyEncryptor import MyEncryptor
-# sample string to encode
 
+# sample string to encode
 stringToEncode = "secret"
 # sample file to encode
 fileToEncode = "sample_file_for_lab2.txt"
 
 # INSTANTIATE THE MyEncryptor Class
 lab1 = MyEncryptor()
-#PRINT OUT TO THE CONSOLE THE VARIOUS HASH VALUES
-print "Encoding the following string %r" %stringToEncode
+# PRINT OUT TO THE CONSOLE THE VARIOUS HASH VALUES by calling the different methods
+
+print "Encoding the following string %r" % stringToEncode
 print "Sha1 file encode : ", lab1.sha1Encode(stringToEncode)
 print "Sha256 version : ", lab1.sha256Encode(stringToEncode)
 print "Sha512 version : ", lab1.sha512Encode(stringToEncode)
-print "Sha3 512 bit version OF FILE : ", lab1.sha3Encode(fileToEncode)
+print "Sha3 256 bit version OF FILE : ", lab1.sha3_256Encode(fileToEncode)
+print "Sha3 512 bit version OF FILE : ", lab1.sha3_512Encode(fileToEncode)
